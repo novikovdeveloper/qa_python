587c472b96116a2125f8d4398b4f92dbebd98a2d
Denis Novikov <amxlife@mail.ru>
2023-04-09 13:57:32 +0300

    Sprint_2

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from main import BooksCollector
+import pytest
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -20,5 +21,75 @@ class TestBooksCollector:
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+
+    # Проверка добавления книг.
+
+    def test_add_new_book(self):
+        new_book = BooksCollector()
+        new_book.add_new_book('Гордость и предубеждение и зомби')
+        assert len(new_book.get_books_rating()) == 1
+
+
+    # Нельзя добавить одну и ту же книгу дважды.
+    def test_cant_add_same_book_twice(self):
+        twice_book = BooksCollector()
+        twice_book.add_new_book('Гордость и предубеждение и зомби')
+        twice_book.add_new_book('Гордость и предубеждение и зомби')
+        twice_book.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(twice_book.get_books_rating()) == 2
+
+    # Нельзя выставить рейтинг книге, которой нет в списке.
+    def test_cant_rate_book_not_on_list(self):
+        not_in_lst = BooksCollector()
+        not_in_lst.add_new_book('Гордость и предубеждение и зомби')
+        not_in_lst.set_book_rating('Бойцовский клуб', 9)
+        assert not_in_lst.books_rating == {'Гордость и предубеждение и зомби': 1}
+
+    # Нельзя выставить рейтинг меньше 1.
+    def test_cant_rate_less_one(self):
+        books_rate = BooksCollector()
+        books_rate.add_new_book('Гордость и предубеждение и зомби')
+        books_rate.set_book_rating('Гордость и предубеждение и зомби', 0)
+        assert books_rate.books_rating != {'Гордость и предубеждение и зомби', 0}
+
+    # Нельзя выставить рейтинг больше 10.
+    def test_cant_rate_more_ten(self):
+        books_rate = BooksCollector()
+        books_rate.add_new_book('Гордость и предубеждение и зомби')
+        books_rate.set_book_rating('Гордость и предубеждение и зомби', 13)
+        assert books_rate.books_rating != {'Гордость и предубеждение и зомби', 13}
+
+    # У не добавленной книги нет рейтинга.
+    def test_absent_book_has_no_rating(self):
+        absent_book = BooksCollector()
+        absent_book.add_new_book('Гордость и предубеждение и зомби')
+        rating = absent_book.get_book_rating('Бойцовский клуб')
+        assert rating is None
+
+    # Добавление книги в избранное
+    def test_add_boot_to_favorites(self):
+        favorite_book = BooksCollector()
+        favorite_book.add_new_book('Гордость и предубеждение и зомби')
+        favorite_book.add_book_in_favorites('Гордость и предубеждение и зомби')
+        assert favorite_book.favorites == ['Гордость и предубеждение и зомби']
+
+
+
+    # Нельзя добавить книгу в избранное, если её нет в словаре books_rating
+
+    def test_absent_book_has_no_favorites(self):
+        no_rate_book = BooksCollector()
+        no_rate_book.add_new_book('Гордость и предубеждение и зомби')
+        without_rating = no_rate_book.get_book_rating('Бойцовский клуб')
+        assert without_rating is None
+
+    # Проверка удаления книги из избранного.
+
+    def test_remove_book_from_favorites(self):
+        del_book = BooksCollector()
+        del_book.add_new_book('Гордость и предубеждение и зомби')
+        del_book.add_book_in_favorites('Гордость и предубеждение и зомби')
+        none_lst = del_book.delete_book_from_favorites('Гордость и предубеждение и зомби')
+        assert none_lst is None
+
