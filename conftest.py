10b8490e2b5e39b4600ee65e047e2fefabb97c87
Denis Novikov <amxlife@mail.ru>
2023-04-21 23:19:00 +0300

    Sprint_2_refactor_fixture_and_tests_2

--- conftest.py
+++ conftest.py
@@ -30,25 +30,4 @@ def book_of_war():
     return 'Искусство войны'
 
 
-@pytest.fixture()
-def add_book_of_war(collector):
-    collector.add_new_book('Искусство войны')
-    return add_book_of_war
 
-
-@pytest.fixture()
-def add_book_of_zombie(collector):
-    collector.add_new_book('Гордость и предубеждение и зомби')
-    return add_book_of_zombie
-
-
-@pytest.fixture()
-def add_book_of_cat(collector):
-    collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-    return add_book_of_cat
-
-
-@pytest.fixture()
-def add_book_of_fight(collector):
-    collector.add_new_book('Бойцовский клуб')
-    return add_book_of_fight
--- tests.py
+++ tests.py
@@ -23,42 +23,49 @@ class TestBooksCollector:
         collector.set_book_rating('Бойцовский клуб', 9)
         assert collector.get_books_rating() == {}
 
-    def test_cant_rate_less_one(self, collector, add_book_of_zombie, book_of_zombie):
+    def test_cant_rate_less_one(self, collector, book_of_zombie):
+        collector.add_new_book(book_of_zombie)
         collector.set_book_rating(book_of_zombie, 0)
         collector.set_book_rating(book_of_zombie, 1)
         assert collector.get_book_rating(book_of_zombie) == 1, "Рейтинг книги можно установить менее 1"
 
-    @pytest.mark.parametrize('book_rate', ['Гордость и предубеждение и зомби', 'Гордость и предубеждение и зомби'])
-    def test_cant_rate_more_ten(self, collector, add_book_of_zombie, book_rate):
-        collector.set_book_rating(book_rate, 11)
-        collector.set_book_rating(book_rate, 10)
-        assert collector.get_book_rating(
-            'Гордость и предубеждение и зомби') == 10, "Можно установить рейтинг книги больше 10"
+    def test_cant_rate_more_ten(self, collector, book_of_zombie, book_of_war):
+        collector.add_new_book(book_of_zombie)
+        collector.add_new_book(book_of_war)
+        collector.set_book_rating(book_of_war, 11)
+        collector.set_book_rating(book_of_zombie, 10)
+        assert len(collector.get_books_with_specific_rating(10)) == 1, "Можно установить рейтинг книги больше 10"
 
-    def test_absent_book_has_no_rating(self, collector, add_book_of_war, book_of_war):
+    def test_absent_book_has_no_rating(self, collector, book_of_war):
+        collector.add_new_book(book_of_war)
         collector.set_book_rating(book_of_war, 2)
         assert 'Бойцовский клуб' not in collector.get_books_rating(), 'Не добавленной книге присвоен рейтинг'
 
-    def test_add_book_to_favorites(self, collector, add_book_of_zombie, book_of_zombie):
+    def test_add_book_to_favorites(self, collector, book_of_zombie):
+        collector.add_new_book(book_of_zombie)
         collector.add_book_in_favorites(book_of_zombie)
         assert len(collector.get_list_of_favorites_books()) == 1
         assert 'Гордость и предубеждение и зомби' in collector.get_list_of_favorites_books(), "Книга не добавилась в Избранное"
 
     def test_absent_book_has_no_favorites(self, collector, book_of_war):
         collector.add_book_in_favorites(book_of_war)
-        assert 'Искусство войны' not in collector.favorites, "Несуществующая книга добавилась в Избранное"
+        assert 'Искусство войны' not in collector.get_list_of_favorites_books(), "Несуществующая книга добавилась в Избранное"
 
     def test_remove_book_from_favorites(self, collector, book_of_zombie):
         collector.add_book_in_favorites(book_of_zombie)
         collector.delete_book_from_favorites(book_of_zombie)
         assert len(collector.get_list_of_favorites_books()) == 0, "Книга не удалилась из избранного"
 
-    def test_get_books_with_specific_rating(self, collector,add_book_of_zombie, add_book_of_cat, add_book_of_fight, book_of_zombie, book_of_cat, book_of_fight):
+    def test_get_books_with_specific_rating(self, collector, book_of_zombie, book_of_cat, book_of_fight):
+        collector.add_new_book(book_of_zombie)
+        collector.add_new_book(book_of_cat)
+        collector.add_new_book(book_of_fight)
         collector.set_book_rating(book_of_zombie, 1)
         collector.set_book_rating(book_of_cat, 6)
         collector.set_book_rating(book_of_fight, 10)
         assert collector.get_books_with_specific_rating(10) == ['Бойцовский клуб'], "Вернулись книги не с тем рейтингом"
 
-    def test_set_book_rating(self, collector, add_book_of_war, book_of_war):
+    def test_set_book_rating(self, collector, book_of_war):
+        collector.add_new_book(book_of_war)
         collector.set_book_rating(book_of_war, 8)
         assert collector.get_books_rating() == {'Искусство войны': 8}, "Рейтинг книги не равен установленному"
